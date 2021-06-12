35114a46db9e3b743bf5a8d3245be361b61a102e
Victor Cubiles <victordnb@gmail.com>
2021-06-13 01:14:58 +0200

    Tabla en proceso, solo se ha modificado los nombres de los atributos se ha añadido.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 //import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import "@fontsource/roboto";
+import Button from "@material-ui/core/Button";
+import Orders from "./components/dashboard";
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,10 +13,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Button variant="Pruebatext" color="red">
+          Hola
+          
+        </Button>
+        <Orders color= "red">
+
+        </Orders>
         
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
