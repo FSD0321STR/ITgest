a8c82f5032af1834ba7b967cf5a0502e87928625
Asier Jimenez Sanchez <operaciones@Air-de-Asier.home>
2021-06-15 11:55:02 +0200

    encuadrado

--- src/pages/user-manager.jsx
+++ src/pages/user-manager.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
-import { TextField, Button, Select, MenuItem, makeStyles, colors } from "@material-ui/core";
+import { TextField, Button, Select, MenuItem, makeStyles, FormHelperText, Grid } from "@material-ui/core";
 import roles from "../contexts/roles";
 import { red } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '40ch',
-        display: 'block',
-        left: '70%',
+        display: 'grid',
+        gridTemplateColumns: '50% 50%',
+        marginTop: '20px'
     },
-    },
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        
+
+    }
 }));
 
 
@@ -50,20 +53,24 @@ function userManager(){
         border: 'solid red'
     }
 
+
     return(
-        <form className={classes.root} noValidate autoComplete="off" >
-            <TextField id="nameImput" label="Nombre" type="text" variant="outlined" value={(e)=>setName(event.target.value)} />
-            <TextField id="surnameImput" label="Apellidos" type="text" variant="outlined" value={(e)=>setSurname(e.target.value)} />
-            <TextField id="emailImput" label="Email" type="text" variant="outlined" onChange = {(e)=>emailverification(e.target.value)} />
-            {errorMessage && <div style={errorStyle}> {errorMessage} </div>}
-            <TextField id="passwordImput" label="Contraseña" type="text" variant="outlined" value={(e)=>setPassword(e.target.value)} />
-            <Select  labelId="demo-simple-select-label" id="roleImput" variant="outlined" label="Rol" value={role} onChange={handleChange} helperText="Por favor, seleccione una categoría para el producto">
-                {Object.values(roles).map((rol) => (
-                    <MenuItem  key={rol} value={rol}>{rol}</MenuItem>
-                ))} 
-            </Select>
-            <Button  variant="contained" color="primary">Crear Usuario</Button>
-        </form>
+        <div className={classes.root} >
+            <div></div>
+            <form className= {classes.form} noValidate autoComplete="off" >
+                <TextField id="nameImput" label="Nombre" type="text" variant="outlined" value={(e)=>setName(event.target.value)} />
+                <TextField id="surnameImput" label="Apellidos" type="text" variant="outlined" value={(e)=>setSurname(e.target.value)} />
+                <TextField id="emailImput" label="Email" type="text" variant="outlined" onChange = {(e)=>emailverification(e.target.value)} />
+                <FormHelperText>Error</FormHelperText>
+                <TextField id="passwordImput" label="Contraseña" type="text" variant="outlined" value={(e)=>setPassword(e.target.value)} />
+                <Select  labelId="demo-simple-select-label" id="roleImput" variant="outlined" label="Rol" value={role} onChange={handleChange} helperText="Por favor, seleccione una categoría para el producto">
+                    {Object.values(roles).map((rol) => (
+                        <MenuItem  key={rol} value={rol}>{rol}</MenuItem>
+                    ))} 
+                </Select>
+                <Button  variant="contained" color="primary">Crear Usuario</Button>
+            </form>
+        </div>
 );
 };
 
