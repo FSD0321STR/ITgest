3f9c86e5256b64c55ed6296063cb9cfb4523e910
Asier Jimenez Sanchez <jimenezsanchezasier@gmail.com>
2021-06-23 21:03:16 +0200

    creando en componentes

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,25 +1,71 @@
 import React from "react";
-import Button from '@material-ui/core/Button'
+import {
+  Container,
+  Button,
+  makeStyles,
+  Grid,
+  Box,
+  Fab,
+} from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
+import DeleteIcon from "@material-ui/icons/Delete";
+import AddIcon from "@material-ui/icons/Add";
 
+const useStyles = makeStyles((theme) => ({
+  options: {
+    display: "flex",
+    marginRight: "20px",
+  },
+}));
 
+function UserList() {
+  const classes = useStyles();
+  const users = {
+    user1: { email: "asier.j.ehu@gmail.com", id: 1 },
+    user2: { email: "asier.j.ehu+2@gmail.com", id: 2 },
+    user3: { email: "asier.j.ehu+3@gmail.com", id: 3 },
+  };
 
-function UserList(){
-    const users = {
-        user1:
-            {email : 'asier.j.ehu@gmail.com',
-            id: 1},
-        user2:
-            {email : 'asier.j.ehu+2@gmail.com',
-            id: 2},
-        user3:
-            {email : 'asier.j.ehu+3@gmail.com',
-            id: 3}
+  const deleteUser = (e) => {
+    const userId = e.target.value;
+    console.log();
+  };
 
-    }
+  const editUser = () => {
+    console.log("editUser");
+  };
 
-    return(
-        <Button variant="contained" color="primary">{users.email}</Button>
-    )
+  return (
+    <div>
+      <Grid item xs={14} sm={10}>
+        <Box bgcolor="secondary.main" color="secondary.contrastText" p={2}>
+          {users.user1.email}
+          <div>
+            <Tooltip title="Delete">
+              <IconButton
+                onClick={deleteUser}
+                value={users.user1.id}
+                aria-label="delete"
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip
+              onClick={editUser}
+              value={users.user1.id}
+              title="Edit"
+              aria-label="add"
+            >
+              <Fab color="primary">
+                <AddIcon />
+              </Fab>
+            </Tooltip>
+          </div>
+        </Box>
+      </Grid>
+    </div>
+  );
 }
 
 export default UserList;
--- src/pages/user-manager.jsx
+++ src/pages/user-manager.jsx
@@ -1,17 +1,7 @@
 import React, { useState } from "react";
-import {
-  TextField,
-  Button,
-  Select,
-  MenuItem,
-  makeStyles,
-  FormHelperText,
-  Grid,
-} from "@material-ui/core";
-import roles from "../contexts/roles";
-import UserList from "../components/UserList";
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
+import { makeStyles } from "@material-ui/core";
+import UserList from "../components/userList";
+import UserForm from "../components/userForm";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,132 +9,19 @@ const useStyles = makeStyles((theme) => ({
     gridTemplateColumns: "50% 50%",
     marginTop: "20px",
   },
-  form: {
-    display: "flex",
-    flexDirection: "column",
-    marginRight: "20px",
-  },
-  item: {
-    marginTop: "20px",
-  },
 }));
 
 function userManager() {
   const classes = useStyles();
-  const [role, setRole] = useState(roles.R1);
-
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-
-  const handleChange = (event) => {
-    setRole(event.target.value);
-  };
-
-  const createUser = (data) => {
-    const newUser = {
-      email: data.email,
-      password: data.password,
-      name: data.name,
-      surname: data.surname,
-      role: role,
-    };
-    console.log(newUser);
-  };
 
   return (
     <div className={classes.root}>
       <div>
         <UserList />
       </div>
-      <form
-        onSubmit={handleSubmit(createUser)}
-        className={classes.form}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          className={classes.item}
-          {...register("name", { required: true })}
-          label="Nombre"
-          type="text"
-          name="name"
-          variant="outlined"
-          error={errors.name}
-        />
-        <TextField
-          className={classes.item}
-          {...register("surname", { required: true })}
-          label="Apellidos"
-          type="text"
-          name="surname"
-          variant="outlined"
-          error={errors.surname}
-        />
-        <TextField
-          className={classes.item}
-          id="emailImput"
-          label="Email"
-          name="email"
-          {...register("email", {
-            required: "Email no valido",
-            pattern: /^[a-z0-9._%+-]+@[a-z0-9,-]+\.[a-z]{2,4}$/,
-          })}
-          type="email"
-          variant="outlined"
-          error={errors.email}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="email"
-          render={({ message }) => <p>{message}</p>}
-        />
-
-        <TextField
-          className={classes.item}
-          {...register("password", {
-            required: "La contraseña debe terner al menos 6 caracteres",
-            minLength: 6,
-          })}
-          label="Contraseña"
-          type="password"
-          name="password"
-          variant="outlined"
-          error={errors.password}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="password"
-          render={({ message }) => <p>{message}</p>}
-        />
-
-        <Select
-          className={classes.item}
-          labelId="demo-simple-select-label"
-          id="roleImput"
-          name="role"
-          variant="outlined"
-          label="Rol"
-          value={role}
-          onChange={handleChange}
-        >
-          {Object.values(roles).map((rol) => (
-            <MenuItem key={rol} value={rol}>
-              {rol}
-            </MenuItem>
-          ))}
-        </Select>
-        <Button
-          type="submit"
-          className={classes.item}
-          variant="contained"
-          color="primary"
-        >
-          Crear Usuario
-        </Button>
-      </form>
+      <div>
+        <UserForm />
+      </div>
     </div>
   );
 }
