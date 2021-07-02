d078a7d7b4e3bda7b12956bb1a67ca2fabb96932
SoufianY <soufian.younoussi@gmail.com>
2021-07-02 20:59:31 +0200

    Form conect

---
+++ src/pages/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+  
+
+export const NavBar = () => {
+    return (
+        <div>
+            <Button variant="outlined" color="primary"> 
+                <Link to="/product"> Producto </Link>
+            </Button>
+        </div>
+    )
+}
