f99204657c18fe094a9b4737cd364ddce0290b43
Soufian Younoussi Azibi <Soufian@Xandebook.local>
2021-07-09 11:49:42 +0200

    ok

--- src/components/TagBar.jsx
+++ src/components/TagBar.jsx
@@ -1,19 +1,18 @@
-import React, {useState } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-// import MediaCard from './Product'
-import { Button } from '@material-ui/core';
-// import MultilineTextFields from './FormCreateProduct'
-// import AppRouters from '../routers/AppRouters'
 
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+// import MediaCard from './Product'
+import { Button } from "@material-ui/core";
+// import MultilineTextField from './FormCreateProduct'
+import Orders from "./dashboard";
 
 // import CollapsibleTable from 'TableStock';
-
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -44,7 +43,11 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
+
+    "aria-controls": `simple-tabpanel-${index}`,
+
     'aria-controls': `simple-tabpanel-${index}`,
+
   };
 }
 
@@ -59,16 +62,20 @@ export default function SimpleTabs() {
   const classes = useStyles();
 
   const [value, setValue] = useState(0);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const [close, setClose] = useState();
-  const handelClose = () => {
-    setClose
-  };
 
   return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
     <div className={classes.root}>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
@@ -78,38 +85,16 @@ export default function SimpleTabs() {
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-          
+        Stock
+        <Orders />
       </TabPanel>
-      <TabPanel value={value} index={1}>
-
-
-
-
-        
-
-
-      </TabPanel>
+      <TabPanel value={value} index={1}></TabPanel>
 
       <TabPanel value={value} index={2}>
-      Productos de tu App
+        {/* Productos de tu App
+        
+        <MediaCard /> */}
 
-      <br />
-      <br />
-      <br />
-
-      <Button value="close" size="large" variant="contained" color="primary" onChange={handelClose} >
-           Crear producto 
-      </Button>
-      <br />
-      <br />
-
-      <div id="form">
-        {/* <MultilineTextFields /> */}
-        {/* <AppRouters /> */}
-      </div>
-
-
-      
       </TabPanel>
     </div>
   );
