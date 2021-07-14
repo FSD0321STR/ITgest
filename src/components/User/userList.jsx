54f336866670e2e7d26c1042cd9f6bbd042d186b
Asier Jimenez Sanchez <jimenezsanchezasier@gmail.com>
2021-07-14 22:12:54 +0200

    .

--- src/components/User/userList.jsx
+++ src/components/User/userList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { makeStyles } from "@material-ui/core";
+import { TabScrollButton } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    padding: "20px",
+  },
+}));
 function UserList({ children }) {
-  return <div>{children}</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>{children}</div>;
 }
-
 export default UserList;
