b046b0be540c1fe31092f2b3416e271cc51853a8
Soufian Younoussi <soufianyounoussi@MacBook-Pro-de-Soufian.local>
2021-07-16 00:09:12 +0200

    scroll en usuarios creados

--- src/components/User/userList.jsx
+++ src/components/User/userList.jsx
@@ -3,8 +3,17 @@ import { makeStyles } from "@material-ui/core";
 import { TabScrollButton } from "@material-ui/core";
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
+    maxHeight: "600px",
+    borderRadius: "10px",
+    marginTop:"30px",
+    marginLeft:"20px",
+    boxShadow: "2px 2px 15px #8888",
+    width: "90%",
     padding: "20px",
+    overflowY: "scroll",
+    scrollBehavior: "smooth",
+    
+    
   },
 }));
 function UserList({ children }) {
